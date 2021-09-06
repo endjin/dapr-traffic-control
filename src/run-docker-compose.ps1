acdf9539828e61c26f868348e4782bf4a1e53119
James Dawson <james.dawson@endjin.com>
2021-09-06 17:53:31 +0100

    Fixed incorrect path when building the dapr-config container

--- src/run-docker-compose.ps1
+++ src/run-docker-compose.ps1
@@ -1,7 +1,7 @@
 docker build -t src_tcs ./TrafficControlService
 docker build -t src_vrs ./VehicleRegistrationService
 docker build -t src_fcs ./FineCollectionService
-docker build -t src_dapr-config ./dapr
+docker build -t src_dapr-config ./dapr-compose
 docker build -t src_dtc-mosquitto ./Infrastructure/mosquitto
 
 # launch services
@@ -9,4 +9,9 @@ docker-compose up -d
 
 # launch visual simulation
 push-location ./VisualSimulation
-dotnet run .\VisualSimulation.csproj
+try {
+    dotnet run .\VisualSimulation.csproj
+}
+finally {
+    Pop-Location
+}
